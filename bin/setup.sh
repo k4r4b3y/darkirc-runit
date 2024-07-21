20812971d16cc57280a5547994714d49b7d64496
k4r4b3y <user@host.localdomain>
2024-07-21 23:18:58 +0300

    Write in pseudo code what the upcoming shell script should do

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+runit_dir="${HOME}/.config/sv"
 pkg upgrade -y
+pkg install termux-services -y
+
+# if ${runit_dir} does NOT exist
+# then create ${runit_dir}
+# else do nothing
+
+# mv bin/darkirc -t ${runit_dir}
