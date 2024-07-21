ac667b122a6f74996622d1720c83ba3ecf1337ae
k4r4b3y <user@host.localdomain>
2024-07-21 23:44:03 +0300

    Write documentation and add todo items

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
-runit_dir="${HOME}/.config/sv"
 pkg upgrade -y
 pkg install termux-services -y
+homelocalbin="${HOME}/.local/bin"
+runit_dir="${HOME}/.config/sv"
+
+# if ${homelocaldir} does NOT exist
+# then create ${homelocaldir}
+# else do nothing
 
 # if ${runit_dir} does NOT exist
 # then create ${runit_dir}
 # else do nothing
 
-# mv bin/darkirc -t ${runit_dir}
+# cp bin/darkirc -t ${runit_dir}
+# you need to make the runit scripts executable as well
+
+# cp bin/darkirc.aarch64-android -t ${homelocalbin}
+# chmod u+x ${homelocalbin}/darkirc.aarch64-android
