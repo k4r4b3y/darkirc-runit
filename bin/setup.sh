80e30bb734c2cf63645f58b273002241a4352385
k4r4b3y <user@host.localdomain>
2024-07-22 16:56:23 +0300

    Use full paths

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,7 @@
 pkg upgrade -y
 pkg install termux-services -y
 
+repo_dir="${HOME}/darkirc-runit"
 homelocalbin_dir="${HOME}/.local/bin"
 runit_dir="${HOME}/.config/sv"
 darkirc_bin="darkirc.aarch64-android"
@@ -16,10 +17,10 @@ if [ ! -d "${runit_dir}" ]; then
   mkdir -p "${runit_dir}"
 fi
 
-cp -r bin/darkirc -t ${runit_dir}
+cp -r ${repo_dir}/bin/darkirc -t ${runit_dir}
 chmod u+x ${runit_dir}/darkirc/run ${runit_dir}/darkirc/log/run
 
-cp -r bin/${darkirc_bin} -t ${homelocalbin_dir}
+cp -r ${repo_dir}/bin/${darkirc_bin} -t ${homelocalbin_dir}
 chmod u+x ${homelocalbin_dir}/${darkirc_bin}
 
 if [ ! -f "${darkirc_conf}" ]; then
