03e5a606fb2aa160ef9a6aa47b04e116e9ac9400
k4r4b3y <user@host.localdomain>
2024-07-22 13:03:46 +0300

    Add runit files

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,19 +1,29 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg upgrade -y
 pkg install termux-services -y
-homelocalbin="${HOME}/.local/bin"
+
+homelocalbin_dir="${HOME}/.local/bin"
 runit_dir="${HOME}/.config/sv"
+darkirc_bin="darkirc.aarch64-android"
+darkirc_conf="${HOME}/.config/darkfi/darkirc_config.toml"
+svdir="${PREFIX}/var/service"
 
-# if ${homelocaldir} does NOT exist
-# then create ${homelocaldir}
-# else do nothing
+if [ ! -d "${homelocalbin_dir}" ]; then
+  mkdir -p "${homelocalbin_dir}"
+fi
 
-# if ${runit_dir} does NOT exist
-# then create ${runit_dir}
-# else do nothing
+if [ ! -d "${runit_dir}" ]; then
+  mkdir "${runit_dir}"
+fi
 
-# cp bin/darkirc -t ${runit_dir}
-# you need to make the runit scripts executable as well
+cp bin/darkirc -t ${runit_dir}
+chmod u+x ${runit_dir}/darkirc/run ${runit_dir}/darkirc/log/run
 
-# cp bin/darkirc.aarch64-android -t ${homelocalbin}
-# chmod u+x ${homelocalbin}/darkirc.aarch64-android
+cp bin/${darkirc_bin} -t ${homelocalbin_dir}
+chmod u+x ${homelocalbin_dir}/${darkirc_bin}
+
+if [ ! -f "${darkirc_conf}" ]; then
+  ${homelocalbin_dir}/${darkirc_bin}
+fi
+
+ln -sf ${runit_dir} ${svdir}/
